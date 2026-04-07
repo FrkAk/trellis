FrkAk/mymir

feat: init repo

mcp/src/state.ts
@@ -1,0 +1,34 @@
+let currentProjectId: string | null = null;
+
+/**
+ * Get the current project ID.
+ * @returns The current project ID.
+ * @throws If no project is selected.
+ */
+export function getProjectId(): string {
+  if (!currentProjectId) {
+    throw new Error(
+      "No project selected. Call trellis_project with action='select' first, or pass projectId explicitly.",
+    );
+  }
+  return currentProjectId;
+}
+
+/**
+ * Set the current project ID for session-scoped tools.
+ * @param id - UUID of the project.
+ */
+export function setProjectId(id: string): void {
+  currentProjectId = id;
+}
+
+/**
+ * Resolve a project ID: use explicit if provided, otherwise fall back to session state.
+ * @param explicit - Optional explicit project ID override.
+ * @returns Resolved project ID.
+ * @throws If neither explicit nor session project ID is available.
+ */
+export function resolveProjectId(explicit?: string): string {
+  if (explicit) return explicit;
+  return getProjectId();
+}

mcp/src/tools/context.ts
@@ -1,0 +1,46 @@
+import { z } from "zod/v4";
+import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { DESCRIPTIONS, handleContext } from "@/lib/ai/tool-handlers";
+import { resolveProjectId } from "../state.js";
+import { json, error } from "./helpers.js";
+
+/**
+ * Register the trellis_context tool on the MCP server.
+ * @param server - The MCP server instance.
+ */
+export function registerContextTool(server: McpServer): void {
+  server.registerTool(
+    "trellis_context",
+    {
+      description: DESCRIPTIONS.trellis_context,
+      inputSchema: z.object({
+        taskId: z.string().describe("Task UUID"),
+        depth: z.enum(["summary", "working", "agent", "planning"]).default("working")
+          .describe("summary=quick, working=detailed, agent=multi-hop for coding, planning=spec for pre-implementation"),
+        projectId: z.string().optional()
+          .describe("Project UUID (uses current if omitted, needed for 'working' depth)"),
+      }),
+      annotations: {
+        title: "Get Task Context",
+        readOnlyHint: true,
+        destructiveHint: false,
+        idempotentHint: true,
+        openWorldHint: false,
+      },
+    },
+    async ({ taskId, depth, projectId }) => {
+      try {
+        const pid = depth === "working" ? resolveProjectId(projectId) : projectId;
+        const result = await handleContext({ taskId, depth, projectId: pid });
+        if (!result.ok) return error(result.error);
+        // Text depths return pre-formatted strings; summary returns JSON
+        if (typeof result.data === "string") {
+          return { content: [{ type: "text" as const, text: result.data }] };
+        }
+        return json(result.data);
+      } catch (e) {
+        return error(e instanceof Error ? e.message : String(e));
+      }
+    },
+  );
+}

mcp/src/tools/query.ts
@@ -1,0 +1,44 @@
+import { z } from "zod/v4";
+import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { DESCRIPTIONS, handleQuery } from "@/lib/ai/tool-handlers";
+import { resolveProjectId } from "../state.js";
+import { json, error } from "./helpers.js";
+
+/**
+ * Register the trellis_query tool on the MCP server.
+ * @param server - The MCP server instance.
+ */
+export function registerQueryTool(server: McpServer): void {
+  server.registerTool(
+    "trellis_query",
+    {
+      description: DESCRIPTIONS.trellis_query,
+      inputSchema: z.object({
+        type: z.enum(["search", "list", "edges", "overview"])
+          .describe("search=find by name or tag, list=all tasks, edges=task relationships, overview=project structure"),
+        query: z.string().optional()
+          .describe("Search string for type='search' — matches against task titles and tags"),
+        taskId: z.string().optional()
+          .describe("Task UUID for type='edges'"),
+        projectId: z.string().optional()
+          .describe("Project UUID (uses current if omitted)"),
+      }),
+      annotations: {
+        title: "Query Tasks",
+        readOnlyHint: true,
+        destructiveHint: false,
+        idempotentHint: true,
+        openWorldHint: false,
+      },
+    },
+    async ({ type, query, taskId, projectId }) => {
+      try {
+        const pid = type !== "edges" ? resolveProjectId(projectId) : projectId;
+        const result = await handleQuery({ type, query, taskId, projectId: pid });
+        return result.ok ? json(result.data) : error(result.error);
+      } catch (e) {
+        return error(e instanceof Error ? e.message : String(e));
+      }
+    },
+  );
+}

middleware.ts
@@ -1,0 +1,61 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const UUID_RE =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+/**
+ * Check Bearer token auth for protected API routes.
+ * Returns null if authorized, or a 401 response if not.
+ * Bypasses: dev mode (no key set), localhost, same-origin browser requests.
+ * @param request - Incoming request.
+ * @returns 401 response or null (authorized).
+ */
+function checkAuth(request: NextRequest): NextResponse | null {
+  const apiKey = process.env.TRELLIS_API_KEY;
+  if (!apiKey) return null;
+
+  const host = request.headers.get("host") ?? "";
+  if (host.startsWith("localhost") || host.startsWith("127.0.0.1"))
+    return null;
+
+  const fetchSite = request.headers.get("sec-fetch-site");
+  if (fetchSite === "same-origin") return null;
+
+  const auth = request.headers.get("authorization");
+  if (auth === `Bearer ${apiKey}`) return null;
+
+  return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+}
+
+/**
+ * Next.js middleware — auth + validation for API routes.
+ * @param request - Incoming request.
+ * @returns Error response or pass-through.
+ */
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname.startsWith("/api/trellis/") || pathname.startsWith("/api/mcp")) {
+    const authError = checkAuth(request);
+    if (authError) return authError;
+  }
+
+  const match = pathname.match(/^\/api\/project\/([^/]+)/);
+  if (match && !UUID_RE.test(match[1])) {
+    return NextResponse.json(
+      { error: "Invalid project ID" },
+      { status: 400 },
+    );
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/api/project/:projectId*",
+    "/api/trellis/:tool*",
+    "/api/mcp",
+  ],
+};

FrkAk/trellis

mcp/src/state.ts
@@ -1,0 +1,34 @@
+let currentProjectId: string | null = null;
+
+/**
+ * Get the current project ID.
+ * @returns The current project ID.
+ * @throws If no project is selected.
+ */
+export function getProjectId(): string {
+  if (!currentProjectId) {
+    throw new Error(
+      "No project selected. Call mymir_project with action='select' first, or pass projectId explicitly.",
+    );
+  }
+  return currentProjectId;
+}
+
+/**
+ * Set the current project ID for session-scoped tools.
+ * @param id - UUID of the project.
+ */
+export function setProjectId(id: string): void {
+  currentProjectId = id;
+}
+
+/**
+ * Resolve a project ID: use explicit if provided, otherwise fall back to session state.
+ * @param explicit - Optional explicit project ID override.
+ * @returns Resolved project ID.
+ * @throws If neither explicit nor session project ID is available.
+ */
+export function resolveProjectId(explicit?: string): string {
+  if (explicit) return explicit;
+  return getProjectId();
+}

mcp/src/tools/context.ts
@@ -1,0 +1,46 @@
+import { z } from "zod/v4";
+import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { DESCRIPTIONS, handleContext } from "@/lib/ai/tool-handlers";
+import { resolveProjectId } from "../state.js";
+import { json, error } from "./helpers.js";
+
+/**
+ * Register the mymir_context tool on the MCP server.
+ * @param server - The MCP server instance.
+ */
+export function registerContextTool(server: McpServer): void {
+  server.registerTool(
+    "mymir_context",
+    {
+      description: DESCRIPTIONS.mymir_context,
+      inputSchema: z.object({
+        taskId: z.string().describe("Task UUID"),
+        depth: z.enum(["summary", "working", "agent", "planning"]).default("working")
+          .describe("summary=quick, working=detailed, agent=multi-hop for coding, planning=spec for pre-implementation"),
+        projectId: z.string().optional()
+          .describe("Project UUID (uses current if omitted, needed for 'working' depth)"),
+      }),
+      annotations: {
+        title: "Get Task Context",
+        readOnlyHint: true,
+        destructiveHint: false,
+        idempotentHint: true,
+        openWorldHint: false,
+      },
+    },
+    async ({ taskId, depth, projectId }) => {
+      try {
+        const pid = depth === "working" ? resolveProjectId(projectId) : projectId;
+        const result = await handleContext({ taskId, depth, projectId: pid });
+        if (!result.ok) return error(result.error);
+        // Text depths return pre-formatted strings; summary returns JSON
+        if (typeof result.data === "string") {
+          return { content: [{ type: "text" as const, text: result.data }] };
+        }
+        return json(result.data);
+      } catch (e) {
+        return error(e instanceof Error ? e.message : String(e));
+      }
+    },
+  );
+}

mcp/src/tools/query.ts
@@ -1,0 +1,44 @@
+import { z } from "zod/v4";
+import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { DESCRIPTIONS, handleQuery } from "@/lib/ai/tool-handlers";
+import { resolveProjectId } from "../state.js";
+import { json, error } from "./helpers.js";
+
+/**
+ * Register the mymir_query tool on the MCP server.
+ * @param server - The MCP server instance.
+ */
+export function registerQueryTool(server: McpServer): void {
+  server.registerTool(
+    "mymir_query",
+    {
+      description: DESCRIPTIONS.mymir_query,
+      inputSchema: z.object({
+        type: z.enum(["search", "list", "edges", "overview"])
+          .describe("search=find by name or tag, list=all tasks, edges=task relationships, overview=project structure"),
+        query: z.string().optional()
+          .describe("Search string for type='search' — matches against task titles and tags"),
+        taskId: z.string().optional()
+          .describe("Task UUID for type='edges'"),
+        projectId: z.string().optional()
+          .describe("Project UUID (uses current if omitted)"),
+      }),
+      annotations: {
+        title: "Query Tasks",
+        readOnlyHint: true,
+        destructiveHint: false,
+        idempotentHint: true,
+        openWorldHint: false,
+      },
+    },
+    async ({ type, query, taskId, projectId }) => {
+      try {
+        const pid = type !== "edges" ? resolveProjectId(projectId) : projectId;
+        const result = await handleQuery({ type, query, taskId, projectId: pid });
+        return result.ok ? json(result.data) : error(result.error);
+      } catch (e) {
+        return error(e instanceof Error ? e.message : String(e));
+      }
+    },
+  );
+}

middleware.ts
@@ -1,0 +1,61 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const UUID_RE =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+/**
+ * Check Bearer token auth for protected API routes.
+ * Returns null if authorized, or a 401 response if not.
+ * Bypasses: dev mode (no key set), localhost, same-origin browser requests.
+ * @param request - Incoming request.
+ * @returns 401 response or null (authorized).
+ */
+function checkAuth(request: NextRequest): NextResponse | null {
+  const apiKey = process.env.MYMIR_API_KEY;
+  if (!apiKey) return null;
+
+  const host = request.headers.get("host") ?? "";
+  if (host.startsWith("localhost") || host.startsWith("127.0.0.1"))
+    return null;
+
+  const fetchSite = request.headers.get("sec-fetch-site");
+  if (fetchSite === "same-origin") return null;
+
+  const auth = request.headers.get("authorization");
+  if (auth === `Bearer ${apiKey}`) return null;
+
+  return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+}
+
+/**
+ * Next.js middleware — auth + validation for API routes.
+ * @param request - Incoming request.
+ * @returns Error response or pass-through.
+ */
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname.startsWith("/api/mymir/") || pathname.startsWith("/api/mcp")) {
+    const authError = checkAuth(request);
+    if (authError) return authError;
+  }
+
+  const match = pathname.match(/^\/api\/project\/([^/]+)/);
+  if (match && !UUID_RE.test(match[1])) {
+    return NextResponse.json(
+      { error: "Invalid project ID" },
+      { status: 400 },
+    );
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/api/project/:projectId*",
+    "/api/mymir/:tool*",
+    "/api/mcp",
+  ],
+};